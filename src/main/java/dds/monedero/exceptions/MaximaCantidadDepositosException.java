085e32ad665246b547470ed34b0954518af0684e
juan-nisimura <juanisimura@gmail.com>
2020-05-06 21:16:12 -0300

    Corrijo formato de MaximaCantidadDepositosException

--- src/main/java/dds/monedero/exceptions/MaximaCantidadDepositosException.java
+++ src/main/java/dds/monedero/exceptions/MaximaCantidadDepositosException.java
@@ -1,9 +1,7 @@
 package dds.monedero.exceptions;
 
 public class MaximaCantidadDepositosException extends RuntimeException {
-
   public MaximaCantidadDepositosException(String message) {
     super(message);
   }
-
 }
